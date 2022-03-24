d52d09bcff7ab794490884d94f07a83011dd1c7b
Davi Silva <davisilvaphoto@gmail.com>
2022-03-24 14:56:36 -0300

    fix music starting

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,12 +1,16 @@
-import { useEffect, useLayoutEffect, useState } from "react";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 import ReactAudioPlayer from "react-audio-player";
-
 import { AiFillInstagram } from "react-icons/ai";
-
 import { Container, ListContainer } from "./styles";
 
 export const Footer = () => {
+  const [volume, setVolume] = useState(0);
+
+  useEffect(() => {
+    setVolume(0.3);
+  }, []);
+
   return (
     <Container>
       <p>Copyright © All rights reserved</p>
@@ -15,7 +19,7 @@ export const Footer = () => {
         src="/assets/audio/audio.mp3"
         autoPlay
         className="audio"
-        volume={0.3}
+        volume={volume}
         controls
         preload="auto"
       />
